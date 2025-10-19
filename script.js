34885e619ef9bd54c4fccc471450310db581de96
corrydautrich <corry.dautrich@icloud.com>
2025-10-19 10:52:35 -0500

    made the formatting nicer in the code

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
 
-let favoriteFoods = ["Donut Holes", "Chicken Tenders", "Mac and Cheese", "Grilled Cheese", "Chicken Alfredo", "Pickles"];
+let favoriteFoods = [
+  "Donut Holes", 
+  "Chicken Tenders", 
+  "Mac and Cheese", 
+  "Grilled Cheese", 
+  "Chicken Alfredo", 
+  "Pickles"
+];
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
 
-for (let food of favoriteFoods) {console.log(`One of my favorite foods is ${food}.`);}
+for (let food of favoriteFoods) {
+  console.log(`One of my favorite foods is ${food}.`);
+}
 
 // 3. Print out the rating for each food with a ranking like:
 // "My #1 favorite food is Ramen" (copy/paste for all items)
 // "My #2 favorite food is Sushi"
 // ...etc.
 
-for (let i = 0; i < favoriteFoods.length; i++) {console.log(`My #${i +1} favorite food is ${favoriteFoods[i]}.`)};
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log(`My #${i +1} favorite food is ${favoriteFoods[i]}.`);
+}
 
 
 // 4a. Create a function printFoodRecommendation(foodName) that prints out the following for the foodName provided
@@ -20,11 +31,17 @@ for (let i = 0; i < favoriteFoods.length; i++) {console.log(`My #${i +1} favorit
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
 
-function printFoodRecommendation(foodName) {console.log(`Have you ever tried ${foodName}?`); console.log(`I always recommend ${foodName} to friends.`); console.log(`Trust me - ${foodName} is delicious.`); console.log("");}
+function printFoodRecommendation(foodName) {
+  console.log(`Have you ever tried ${foodName}?`); 
+  console.log(`I always recommend ${foodName} to friends.`); 
+  console.log(`Trust me - ${foodName} is delicious.`); 
+}
 
 // 4b. Call the function at least 3 times
 
-printFoodRecommendation("Chicken Alfredo"); printFoodRecommendation("Grilled Cheese"); printFoodRecommendation("Donut Holes");
+printFoodRecommendation("Chicken Alfredo"); 
+printFoodRecommendation("Grilled Cheese"); 
+printFoodRecommendation("Donut Holes");
 
 // Here's a list of 50 friends' favorite foods:
 let friendFavorites = [
@@ -52,10 +69,18 @@ let shortFoodNames = friendFavorites.filter(food => food.length <= 6);
 
 console.log("Long food names:", longFoodNames);
 console.log("Short food names:", shortFoodNames);
-if (longFoodNames.length > shortFoodNames.length) {console.log("There are more long-named foods.");} else if (shortFoodNames.length > longFoodNames.length) {console.log("There are more short-named foods.")} else {console.log("There are en equal number of long and short-named foods.");}
+if (longFoodNames.length > shortFoodNames.length) {
+  console.log("There are more long-named foods.");
+} else if (shortFoodNames.length > longFoodNames.length) {
+  console.log("There are more short-named foods.");
+} else {
+  console.log("There are en equal number of long and short-named foods.");
+}
 
 // 10. STRETCH: Find the longest food name and print:
 // "The longest food name in the list is ______ with ___ characters."
 
-let longestFood = friendFavorites.reduce((longest, current) => current.length > longest.length ? current : longest);
+let longestFood = friendFavorites.reduce((longest, current) => 
+  current.length > longest.length ? current : longest
+);
 console.log(`The longest food name in the list is "${longestFood} with ${longestFood.length} characters.`);
